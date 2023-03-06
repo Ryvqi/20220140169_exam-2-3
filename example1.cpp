43c88f07a99c0f53064295ed9d42eb1348e55ea6
Ryvqi <rifqikhabibi03@gmail.com>
2023-03-06 17:39:49 +0700

    first commit

---
+++ example1.cpp
@@ -0,0 +1,24 @@
+//Library
+#include <iostream>
+using namespace std;
+
+//program main
+int main(){//begin
+    
+    //numeric p,l,luas
+    double p,l,luas;
+    //display "masukan panjang"
+    cout<<"masukan panjang = ";
+    //accept p
+    cin>>p;
+    //display "masukan lebar"
+    cout<<"masukan lebar = ";
+    //accept l
+    cin>>l;
+    //compute luas=p*l
+    luas=p*l;
+    //display "Luas persegi panjang adalah", luas
+    cout<<"luas persegi panajng adalah"<<luas;
+
+
+}//end
